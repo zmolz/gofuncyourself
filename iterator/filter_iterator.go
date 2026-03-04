c49f7cce5785e9112c19511ac256a0b82249d2a7
zmolz <zackmoulsdale@gmail.com>
2026-03-04 09:21:19 -0500

    Filter Iterator

--- iterator/filter_iterator.go
+++ iterator/filter_iterator.go
@@ -1,8 +1,11 @@
 package iterator
 
 type FilterIterator[T any] struct {
-	src Iterator[T]
-	f   func(T) bool
+	src     Iterator[T]
+	f       func(T) bool
+	nextVal T
+	nextOk  bool
+	looked  bool
 }
 
 // NewFilterIterator creates a lazy FilterIterator from src Iterator
@@ -14,15 +17,36 @@ func NewFilterIterator[T any](src Iterator[T], f func(T) bool) *FilterIterator[T
 }
 
 func (filter *FilterIterator[T]) HasNext() bool {
-	return filter.src.HasNext()
-}
+	if filter.looked {
+		return filter.nextOk
+	}
 
-func (filter *FilterIterator[T]) Next() (T, bool) {
 	for filter.src.HasNext() {
 		val, _ := filter.src.Next()
 		if filter.f(val) {
-			return val, true
+			filter.nextVal = val
+			filter.nextOk = true
+			filter.looked = true
+			return true
 		}
+	}
+
+	filter.nextOk = false
+	filter.looked = true
+	return false
+}
+
+func (filter *FilterIterator[T]) Next() (T, bool) {
+	if !filter.looked {
+		if !filter.HasNext() {
+			var zero T
+			return zero, false
+		}
+	}
+
+	filter.looked = false
+	if filter.nextOk {
+		return filter.nextVal, true
 	}
 
 	var zero T
